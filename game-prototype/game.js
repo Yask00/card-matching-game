72c38db350f764114d33ad6bda6d6ceaf8dc5073
Yasen Trayanov <yasen.trayanov@industria.tech>
2020-05-17 14:28:56 +0300

    add some comments and cleared some code in  the prototype

--- game-prototype/game.js
+++ game-prototype/game.js
@@ -1,40 +1,48 @@
+/*
+  Notes:
+  Every flip card has data-id:number and data-name:string
+  data-id is to compare two cards and remove their flip listeners
+  data-name is different for every two card pairs, so if one is 
+  ...opened we can check if we are clicking on the same one again
+  lockBoard is true while we are checking unmatched cards and wait them to flip again
+*/
+
 let flipCards = Array.from(document.querySelectorAll(".flipcard"));
 const gameOverlay = document.querySelector(".game-overlay");
 let isUnmatchedCardFlipped = false;
-let matchedCardsCounter = 0;
+let matchedCardsCounter = 0; // ends on 8
 let openedCardOne = null;
 let openedCardTwo = null;
 let lockBoard = false;
 
 function flipListener() {
-  if (!lockBoard && isUnmatchedCardFlipped) {
-    if (
-      openedCardOne.getAttribute("data-name") === this.getAttribute("data-name")
-    ) {
-      return;
+  if (!lockBoard) {
+    if (isUnmatchedCardFlipped) {
+      // flip second card
+      if (
+        openedCardOne.getAttribute("data-name") ===
+        this.getAttribute("data-name")
+      ) {
+        return; // return if we click on the same first open card
+      } else {
+        isUnmatchedCardFlipped = false;
+        openedCardTwo = this;
+
+        const cardBox = this.querySelector(".flipcard-inner");
+        cardBox.classList.toggle("flipped");
+
+        compareTwoCards(openedCardOne, openedCardTwo);
+      }
     } else {
-      isUnmatchedCardFlipped = false;
-      openedCardTwo = this;
+      // flip first card
+      isUnmatchedCardFlipped = true;
+      openedCardOne = this;
 
       const cardBox = this.querySelector(".flipcard-inner");
       cardBox.classList.toggle("flipped");
-
-      compareTwoCards(openedCardOne, openedCardTwo);
     }
-  } else if (!lockBoard) {
-    isUnmatchedCardFlipped = true;
-    openedCardOne = this;
-
-    const cardBox = this.querySelector(".flipcard-inner");
-    cardBox.classList.toggle("flipped");
   }
 }
-
-const listenForFlips = function () {
-  flipCards.forEach(function (card) {
-    card.addEventListener("click", flipListener);
-  });
-};
 
 const compareTwoCards = (card1, card2) => {
   if (card1.getAttribute("data-id") === card2.getAttribute("data-id")) {
@@ -65,7 +73,7 @@ function shuffleCardsArray() {
     [flipCards[i], flipCards[j]] = [flipCards[j], flipCards[i]];
   }
   flipCards.map((card, index) => {
-    card.style.order = index;
+    card.style.order = index; // flex property to mix cards
   });
 }
 
@@ -73,12 +81,13 @@ const startGame = () => {
   gameOverlay.classList.toggle("hidden");
 
   shuffleCardsArray();
-  listenForFlips();
+  flipCards.forEach(function (card) {
+    card.addEventListener("click", flipListener);
+  });
 };
 
 endGame = () => {
   if (matchedCardsCounter === 8) {
-    debugger;
     let flippedCards = Array.from(document.querySelectorAll(".flipcard-inner"));
     flippedCards.forEach((card) => {
       card.classList.remove("flipped");
